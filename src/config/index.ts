ef5c4f58180119706a6ade727e9a97560170bcaa
fanghuan27 <fanghuan27@gmail.com>
2022-09-15 17:26:48 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,14 @@ export const proxy = [
   'https://pikpak.he13797739994.ga',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.go-back.cf',
+  'https://pikpakz.top',
+  'https://wnbh.cf',
+  'https://nsso.eu.org',
+  'https://yooyi.tk',
+  'https://hdddd.me',
+  'https://tjsky-1.cf',
+  'https://tjsky.cf',
+  
 ]
 
 export const version = '1.0.0'
