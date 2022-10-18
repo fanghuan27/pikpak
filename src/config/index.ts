83320cc5ff56cfafae4fa5c2d69a773a3d75a2c1
fanghuan27 <fanghuan27@gmail.com>
2022-10-18 15:32:34 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,14 @@ export const proxy = [
   'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://pikpak.he13797739994.ga',
   'https://pikpak.hdddd.me',
   'https://pan.yooyi.tk',
   'https://pikpak.nsso.eu.org',
-  'https://pikpak-web.wnbh.cf',
   'https://pk.pikpakz.top',
+  'https://cc123.cf',
+  'https://cf-api.xswd.cf',
+  'https://pikpak.arielherself.xyz',
+  'https://f.nxcloud.uk',
+  'https://wkr.btstream.net',
 ]
-
 export const version = '1.0.0'
