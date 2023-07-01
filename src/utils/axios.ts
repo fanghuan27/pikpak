1e86ac105fec879574a7c0f0f96695f197d0bf12
fanghuan27 <fanghuan27@gmail.com>
2023-07-02 01:41:55 +0800

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -32,7 +32,7 @@ instance.interceptors.response.use(response => {
         if(loginDataJson.username && loginDataJson.password && !isLoginLoading) {
           console.log('wait', config.url)
           isLoginLoading = true
-          return instance.post('https://user.mypikpak.net/v1/auth/signin', {
+          return instance.post('https://user.mypikpak.com/v1/auth/signin', {
             "captcha_token": "",
             "client_id": "YNxT9w7GMdWvEOKa",
             "client_secret": "dbw2OtmVEeuUvIptb1Coyg",
