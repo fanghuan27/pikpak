b3db436518b8c542a1f26b07336840ddacfbf261
fanghuan27 <fanghuan27@gmail.com>
2023-07-01 16:14:25 +0800

    Update testtest.vue

--- src/views/testtest.vue
+++ src/views/testtest.vue
@@ -24,7 +24,7 @@ import http from '../utils/axios'
       })
   }
   const inviteInfo = () => {
-    http.get('https://api-drive.mypikpak.com/vip/v1/activity/inviteInfo')
+    http.get('https://api-drive.mypikpak.net/vip/v1/activity/inviteInfo')
       .then((res:any) => {
         if(res.data.invited_nums >= 0) {
           window.$message.success('您已成功邀请' + res.data.invited_nums + '人，' + res.data.add_days + '天')
