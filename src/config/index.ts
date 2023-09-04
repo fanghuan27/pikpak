69f7181c1966703f230fb9447837ec13108f71da
fanghuan27 <fanghuan27@gmail.com>
2023-09-04 18:48:17 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,6 @@
 export const proxy = [
-  'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://pk.pikpakz.top',
-  'https://cc123.cf',
   'https://cf-api.xswd.cf',
   'https://wkr.btstream.net',
 ]
