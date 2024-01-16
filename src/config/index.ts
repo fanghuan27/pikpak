4316c22462dd677f2d89a0bea824d03761aa33f9
fanghuan27 <fanghuan27@gmail.com>
2024-01-17 00:10:26 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,5 @@
 export const proxy = [
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://cf-api.xswd.cf',
-  'https://wkr.btstream.net',
 ]
 export const version = '1.0.0'
